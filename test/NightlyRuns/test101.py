4e2a40592b4e5f49c23e209db79521a66ac040ec
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-20 16:56:18 +1100

    i am testing

--- test/NightlyRuns/test101.py
+++ test/NightlyRuns/test101.py
@@ -1,5 +1,6 @@
 #Test Name: SquareShelfConstrainedStressSSA2d
 import pyissm
+import numpy as np
 
 md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 50000)
 md = pyissm.model.param.set_mask(md, 'all', None)
@@ -14,6 +15,9 @@ md.outputdefinition.definitions = [pyissm.model.classes.massfluxatgate(name  = '
                                    pyissm.model.classes.massfluxatgate(name  = 'MassFlux4', profilename  = '../assets/Exp/MassFlux4.exp', definitionstring  ='Outputdefinition4'),
                                    pyissm.model.classes.massfluxatgate(name  = 'MassFlux5', profilename  = '../assets/Exp/MassFlux5.exp', definitionstring  ='Outputdefinition5'),
                                    pyissm.model.classes.massfluxatgate(name  = 'MassFlux6', profilename  = '../assets/Exp/MassFlux6.exp', definitionstring  ='Outputdefinition6')]
+for d in md.outputdefinition.definitions:
+    print(d.name, "segments is nan?", np.isnan(d.segments).all() if hasattr(d, "segments") else None)
+
 md = pyissm.model.execute.solve(md, 'Stressbalance')
 sol = md.results.StressbalanceSolution
 print([a for a in dir(sol) if "Outputdefinition" in a or "MassFlux" in a])
