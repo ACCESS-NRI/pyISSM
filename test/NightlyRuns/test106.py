c051cdba902313cb74634b116f133a9aed56aa93
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-21 16:55:02 +1100

    add new tests

---
+++ test/NightlyRuns/test106.py
@@ -0,0 +1,17 @@
+#Test Name: SquareShelfConstrainedMasstransp2dDG
+import pyissm
+
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 150000)
+md = pyissm.model.mesh.meshconvert(md)
+md = pyissm.model.param.set_mask(md, 'all', None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.cluster.np = 3
+md.masstransport.stabilization = 3
+md.masstransport.spcthickness = md.geometry.thickness
+md = pyissm.model.execute.solve(md, 'Masstransport')
+
+#Fields and tolerances to track changes
+field_names = ['Thickness']
+field_tolerances = [1e-13]
+field_values = [md.results.MasstransportSolution.Thickness]
---
+++ test/NightlyRuns/test108.py
@@ -0,0 +1,16 @@
+#Test Name: SquareShelfConstrainedTherStea
+import pyissm
+
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 180000)
+md = pyissm.model.param.set_mask(md, 'all', None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
+md = md.extrude(3, 1.)
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.timestepping.time_step = 0
+md.cluster.np = 3
+md = pyissm.model.execute.solve(md, 'Thermal')
+#Fields and tolerances to track changes
+field_names = ['Temperature', 'BasalforcingsGroundediceMeltingRate']
+field_tolerances = [1e-13, 1e-5]
+field_values = [md.results.ThermalSolution.Temperature,
+                md.results.ThermalSolution.BasalforcingsGroundediceMeltingRate]
