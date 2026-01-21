c4e9357f35511ae7e8a5621a031fe6ac84a49ad7
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-21 16:35:51 +1100

    .

--- test/NightlyRuns/test103.py
+++ test/NightlyRuns/test103.py
@@ -5,7 +5,7 @@ md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp'
 md = pyissm.model.param.set_mask(md, 'all', None)
 md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
 md = md.extrude(3, 2.)
-md = pyissm.model.param.set_flow_equation(md, 'HO', 'all')
+md = pyissm.model.param.set_flow_equation(md, HO = 'all')
 md.cluster.np = 3
 md.stressbalance.requested_outputs = ['default', 'StressTensorxx', 'StressTensoryy', 'StressTensorzz', 'StressTensorxy', 'StressTensorxz', 'StressTensoryz']
 md = pyissm.model.execute.solve(md, 'Stressbalance')
