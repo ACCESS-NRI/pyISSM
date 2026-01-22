b098875fc9176304193180a1b44c5e708cea5f14
Lawrence Bird <lawrence.bird@anu.edu.au>
2026-01-23 09:25:26 +1100

    Add test306-314

---
+++ test/NightlyRuns/test311.py
@@ -0,0 +1,18 @@
+#Test Name: SquareSheetConstrainedMasstransp3d
+import pyissm
+
+# Parameterise model
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 150000.)
+md = pyissm.model.param.set_mask(md, None, None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareSheetConstrained.py')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md = md.extrude(5, 0.5)
+md.cluster.np = 3
+
+# Execute model
+md = pyissm.model.execute.solve(md, 'Masstransport')
+
+# Fields and tolerances to track changes
+field_names = ['Thickness']
+field_tolerances = [1e-13]
+field_values = [md.results.MasstransportSolution.Thickness]
---
+++ test/NightlyRuns/test313.py
@@ -0,0 +1,24 @@
+#Test Name: SquareSheetConstrainedTherTran
+import pyissm
+
+# Parameterise model
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 180000.)
+md = pyissm.model.param.set_mask(md, None, None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareSheetConstrained.py')
+md = md.extrude(3, 1.)
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.cluster.np = 3
+md.transient.isstressbalance = False
+md.transient.ismasstransport = False
+md.transient.issmb = True
+md.transient.isthermal = True
+md.transient.isgroundingline = False
+
+# Execute model
+md = pyissm.model.execute.solve(md, 'Transient')
+
+# Fields and tolerances to track changes
+field_names = ['Temperature', 'BasalforcingsGroundediceMeltingRate']
+field_tolerances = [1e-13, 1e-13]
+field_values = [md.results.TransientSolution[0].Temperature,
+                md.results.TransientSolution[0].BasalforcingsGroundediceMeltingRate]
