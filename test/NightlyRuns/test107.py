4a0e02b83e3c1acc84a0072c4362f2d1338b7b84
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-22 11:01:01 +1100

    add fixes

--- test/NightlyRuns/test107.py
+++ test/NightlyRuns/test107.py
@@ -5,7 +5,7 @@ md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp'
 md = pyissm.model.param.set_mask(md, 'all', None)
 md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
 md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
-md.extrude(5, 3.)
+md = md.extrude(5, 3.)
 md.cluster.np = 3
 md = pyissm.model.execute.solve(md, 'Masstransport')
 
