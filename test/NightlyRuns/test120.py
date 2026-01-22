bec578c5c931dd48b526ddb9fee1fc917d821267
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-22 14:26:31 +1100

    add test120.py

--- test/NightlyRuns/test120.py
+++ test/NightlyRuns/test120.py
@@ -5,7 +5,7 @@ md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp'
 md = pyissm.model.param.set_mask(md, 'all', None)
 md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
 md = md.extrude(3, 1.)
-md = pyissm.model.param.set_flow_equation(md, 'SSA', 'all')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
 md.timestepping.time_step = 0
 md.initialization.waterfraction = np.zeros(md.mesh.numberofvertices)
 md.initialization.watercolumn = np.zeros(md.mesh.numberofvertices)
