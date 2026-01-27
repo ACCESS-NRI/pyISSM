76db36bceead5e64aebec4f0fbc61277c5142ede
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-01-27 12:29:11 +1100

    add assets and 501

---
+++ test/NightlyRuns/test501.py
@@ -0,0 +1,17 @@
+#Test Name: PigStressSSA2d
+import pyissm
+
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Pig.exp', 20000.)
+md = pyissm.model.param.set_mask(md, '../assets/Exp/PigShelves.exp', '../assets/Exp/PigIslands.exp')
+md = pyissm.model.param.parameterize(md, '../assets/Par/Pig.py')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.cluster.np = 3
+md = pyissm.model.execute.solve(md, 'Stressbalance')
+
+# Fields and tolerances to track changes
+field_names = ['Vx', 'Vy', 'Vel', 'Pressure']
+field_tolerances = [1e-12, 2e-12, 2e-12, 1e-13]
+field_values = [md.results.StressbalanceSolution.Vx,
+                md.results.StressbalanceSolution.Vy,
+                md.results.StressbalanceSolution.Vel,
+                md.results.StressbalanceSolution.Pressure]
