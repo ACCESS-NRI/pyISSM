8142915949a5256de92b4c07eac89a9e09f9f512
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-02-03 12:22:42 +1100

    add autodiff tests

--- test/NightlyRuns/test3015.py
+++ test/NightlyRuns/test3015.py
@@ -1,35 +1,30 @@
 #Test Name: SquareShelfConstrainedMasstransp2dAdolcForwardDifference
 import copy
-from model import *
-from socket import gethostname
-from triangle import *
-from setmask import *
-from parameterize import *
-from setflowequation import *
-from independent import *
-from dependent import *
-from SetIceShelfBC import *
-from solve import *
-from issmgslsolver import issmgslsolver
-
+import pyissm
 
 #This test runs test3015 with autodiff on, and checks that
 #the value of the scalar forward difference match a step-wise differential
 
 #First configure
-md = triangle(model(), '../Exp/Square.exp', 50000.)
-md = setmask(md, 'all', '')
-md = parameterize(md, '../Par/SquareShelfConstrained.py')
-md = setflowequation(md, 'SSA', 'all')
-md.cluster = generic('name', gethostname(), 'np', 1)
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 50000.)
+md = pyissm.model.param.set_mask(md, 'all', None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.cluster.np = 1
 md.masstransport.requested_outputs = ['IceVolume']
-md.verbose = verbose('autodiff', True)
-md.toolkits.DefaultAnalysis = issmgslsolver()
+md.toolkits.DefaultAnalysis = pyissm.tools.config.issm_gsl_solver()
 
 #setup autodiff parameters
 index = 1  #this is the scalar component we are checking against
-md.autodiff.independents = [independent('name', 'md.geometry.thickness', 'type', 'vertex', 'nods', md.mesh.numberofvertices, 'fos_forward_index', index)]
-md.autodiff.dependents = [dependent('name', 'IceVolume', 'type', 'scalar')]
+md.autodiff.independents = pyissm.model.classes.independent()
+md.autodiff.independents.name = md.geometry.thickness
+md.autodiff.independents.type = 'vertex'
+md.autodiff.independents.nods = md.mesh.numberofvertices
+md.autodiff.independents.fos_forward_index = index
+
+md.autodiff.dependents = pyissm.model.classes.dependent()
+md.autodiff.dependents.name = 'IceVolume'
+md.autodiff.dependents.type = 'scalar'
 md.autodiff.driver = 'fos_forward'
 
 #PYTHON: indices start at 0, make sure to offset index
@@ -52,9 +47,9 @@ md.autodiff.isautodiff = False
 md.geometry.thickness[index] = h0
 md.geometry.base = -md.materials.rho_ice / md.materials.rho_water * md.geometry.thickness
 md.geometry.surface = md.geometry.base + md.geometry.thickness
-md = SetIceShelfBC(md)
+md = pyissm.model.bc.set_ice_shelf_bc(md)
 
-md = solve(md, 'Masstransport')
+md = pyissm.model.execute.solve(md, 'Masstransport')
 V0 = md.results.MasstransportSolution.IceVolume
 
 #backward
@@ -63,9 +58,9 @@ md.autodiff.isautodiff = False
 md.geometry.thickness[index] = h2
 md.geometry.base = -md.materials.rho_ice / md.materials.rho_water * md.geometry.thickness
 md.geometry.surface = md.geometry.base + md.geometry.thickness
-md = SetIceShelfBC(md)
+md = pyissm.model.classes.bc.set_ice_shelf_bc(md)
 
-md = solve(md, 'Masstransport')
+md = pyissm.model.execute.solve(md, 'Masstransport')
 V2 = md.results.MasstransportSolution.IceVolume
 
 #compute resulting derivative
@@ -77,9 +72,9 @@ md.autodiff.isautodiff = True
 md.geometry.thickness[index] = h1
 md.geometry.base = -md.materials.rho_ice / md.materials.rho_water * md.geometry.thickness
 md.geometry.surface = md.geometry.base + md.geometry.thickness
-md = SetIceShelfBC(md)
+md = pyissm.model.bc.set_ice_shelf_bc(md)
 
-md = solve(md, 'Masstransport')
+md = pyissm.model.execute.solve(md, 'Masstransport')
 #retrieve directly
 dVdh_ad = md.results.MasstransportSolution.AutodiffJacobian
 
--- test/NightlyRuns/test3119.py
+++ test/NightlyRuns/test3119.py
@@ -1,32 +1,30 @@
-from model import *
-from socket import gethostname
-from triangle import *
-from setmask import *
-from parameterize import *
-from setflowequation import *
-from independent import *
-from dependent import *
-from solve import *
-from issmgslsolver import issmgslsolver
+import pyissm
 
 
 #test reverse scalar vs forward vectorial drivers in ADOLC, using the test3009 setup, equivalent to test109 setup.
-md = triangle(model(), '../Exp/Square.exp', 100000.)
-md = setmask(md, 'all', '')
-md = parameterize(md, '../Par/SquareShelfConstrained.py')
-md = setflowequation(md, 'SSA', 'all')
-md.cluster = generic('name', gethostname(), 'np', 1)
+md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 100000.)
+md = pyissm.model.param.set_mask(md, 'all', None)
+md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
+md = pyissm.model.param.set_flow_equation(md, SSA = 'all')
+md.cluster.np = 1
 
 md.autodiff.isautodiff = True
-md.toolkits.DefaultAnalysis = issmgslsolver()
+md.toolkits.DefaultAnalysis = pyissm.tools.config.issm_gsl_solver()
 
 #first run scalar reverse mode:
-md.autodiff.independents = [independent('name', 'md.geometry.thickness', 'type', 'vertex', 'nods', md.mesh.numberofvertices)]
-md.autodiff.dependents = [dependent('name', 'MaxVel', 'type', 'scalar', 'fos_reverse_index', 1)]
+md.autodiff.independents = pyissm.model.classes.independent()
+md.autodiff.independents.name = md.geometry.thickness
+md.autodiff.independents.type = 'vertex'
+md.autodiff.independents.nods = md.mesh.numberofvertices
+md.autodiff.independents.fos_reverse_index = 1
+
+md.autodiff.dependents = pyissm.model.classes.dependent()
+md.autodiff.dependents.name = 'MaxVel'
+md.autodiff.dependents.type = 'scalar'
+md.autodiff.dependents.fos_reverse_index = 1
 md.autodiff.driver = 'fos_reverse'
 
-md = solve(md, 'Transient')
-
+md = pyissm.model.execute.solve(md, 'Transient')
 #recover jacobian:
 jac_reverse = md.results.TransientSolution[0].AutodiffJacobian
 
