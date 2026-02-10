d270f1922fedae9020845dec9f9f120177a16064
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-02-10 15:28:17 +1100

    add test3019.py

--- test/NightlyRuns/test3019.py
+++ test/NightlyRuns/test3019.py
@@ -2,8 +2,7 @@
 import numpy as np
 import pyissm
 
-
-#test reverse scalar vs forward vectorial drivers in ADOLC, using the test3009 setup, equivalent to test109 setup.
+# test reverse scalar vs forward vectorial drivers in ADOLC, using the test3009 setup, equivalent to test109 setup.
 md = pyissm.model.mesh.triangle(pyissm.model.Model(), '../assets/Exp/Square.exp', 100000.)
 md = pyissm.model.param.set_mask(md, 'all', None)
 md = pyissm.model.param.parameterize(md, '../assets/Par/SquareShelfConstrained.py')
@@ -14,41 +13,50 @@ md.toolkits.DefaultAnalysis = pyissm.tools.config.issm_gsl_solver()
 md.autodiff.isautodiff = True
 md.verbose.autodiff = True
 
-#first run scalar reverse mode:
-md.autodiff.independents = pyissm.model.classes.independent()
-md.autodiff.independents.name = md.geometry.thickness
-md.autodiff.independents.type = 'vertex'
-md.autodiff.independents.nods = md.mesh.numberofvertices
-md.autodiff.independents.fos_reverse_index = 1
+# first run scalar reverse mode:
+indep = pyissm.model.classes.independent()
+indep.name = 'md.geometry.thickness'
+indep.type = 'vertex'
+indep.nods = md.mesh.numberofvertices
+indep.fos_reverse_index = 1
+md.autodiff.independents = [indep]
 
-md.autodiff.dependents = pyissm.model.classes.dependent()
-md.autodiff.dependents.name = 'MaxVel'
-md.autodiff.dependents.type = 'scalar'
-md.autodiff.dependents.fos_reverse_index = 1
+dep = pyissm.model.classes.dependent()
+dep.name = 'MaxVel'
+dep.type = 'scalar'
+dep.nods = md.mesh.numberofvertices
+dep.fos_reverse_index = 1
+md.autodiff.dependents = [dep]
+
 md.autodiff.driver = 'fos_reverse'
 
-
 md = pyissm.model.execute.solve(md, 'Transient')
-#recover jacobian:
+
+# recover jacobian:
 jac_reverse = md.results.TransientSolution[0].AutodiffJacobian
 
-#now run vectorial forward mode
-md.autodiff.independents = pyissm.model.classes.independent()
-md.autodiff.independents.name = md.geometry.thickness
-md.autodiff.independents.type = 'vertex'
-md.autodiff.independents.nods = md.mesh.numberofvertices
-md.autodiff.independents.fov_forward_indices = np.arange(1, md.mesh.numberofvertices + 1)
+# now run vectorial forward mode
+indep = pyissm.model.classes.independent()
+indep.name = 'md.geometry.thickness'
+indep.type = 'vertex'
+indep.nods = md.mesh.numberofvertices
+indep.fov_forward_indices = np.arange(1, md.mesh.numberofvertices + 1)
+md.autodiff.independents = [indep]
 
-md.autodiff.dependents = pyissm.model.classes.dependent()
-md.autodiff.dependents.name = 'MaxVel'
-md.autodiff.dependents.type = 'scalar'
+dep = pyissm.model.classes.dependent()
+dep.name = 'MaxVel'
+dep.type = 'scalar'
+dep.nods = md.mesh.numberofvertices
+md.autodiff.dependents = [dep]
+
 md.autodiff.driver = 'fov_forward'
 
 md = pyissm.model.execute.solve(md, 'Transient')
-#recover jacobian:
+
+# recover jacobian:
 jac_forward = md.results.TransientSolution[0].AutodiffJacobian
 
-#Fields and tolerances to track changes
+# Fields and tolerances to track changes
 field_names = ['Jac Forward', 'Jac Reverse', 'Jac Forward - Reverse']
 field_tolerances = [1e-8, 1e-8, 5e-6]
 field_values = [jac_forward, jac_reverse, jac_forward - jac_reverse]
