a054bbff1cc79e1d99c8ce430d0f1b399aa1974a
Lawrence Bird <lawrence.bird@anu.edu.au>
2026-02-24 15:50:29 +1100

    Remove interface/ directory

--- interface/README.md
+++
@@ -1,2 +0,0 @@
-## ISSM Python and MATLAB interfaces
-Directory to house existing ISSM Python and MATLAB functionality for integration into pyISSM
--- interface/python_files/src/m/inversions/supportedcostfunctions.py
+++
@@ -1,2 +0,0 @@
-def supportedcostfunctions():
-    return list(range(101, 105 + 1)) + [201] + list(range(501, 508 + 1)) + [510] + list(range(601, 604 + 1))
